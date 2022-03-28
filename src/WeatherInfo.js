fd19fa22fd2a39f7f5fc3bd8c3fecad9e4e4c55f
Lizeth Sandoval Valladares <liz.sandoval09@gmail.com>
2022-03-27 18:22:24 -0700

    Added files.

---
+++ src/WeatherInfo.js
@@ -0,0 +1,36 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1> {props.data.city} </h1>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <div className="d-flex justify-content-start">
+              <WeatherIcon code={props.data.icon} />
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <div className="d-flex justify-content-end">
+            <ul>
+              <li> Wind: {Math.round(props.data.windy)} mph </li>
+              <li> Humidity: {props.data.humidity}% </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
